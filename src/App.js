c47c9fcafde51dc85dc23202edaa7d2af2fec794
alex <casanas10@gmail.com>
2019-10-12 13:09:58 -0400

    added Material UI for creating faster UI components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 const App = () => {
-  return <h1>My News</h1>;
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <h1>My News</h1>
+    </>
+  );
 };
 
 export default App;
